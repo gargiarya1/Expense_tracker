9c0d824109a50a48d2ae82adcb1c37502b452a46
gargiarya1 <95920304+gargiarya1@users.noreply.github.com>
2025-04-24 01:56:16 +0530

    .

---
+++ Expense tracker/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using MessagePack;
+
+namespace Expense_tracker.Models
+{
+    public class User
+    {
+        int name { get; set; }
+        int username { get; set; }
+        int password { get; set; } 
+
+    }
+}
